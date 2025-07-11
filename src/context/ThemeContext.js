0ee3b335d5047c19232eec5b40ff94a1a58ac2ad
Choyx58 <156706031+Choyx58@users.noreply.github.com>
2025-07-11 03:13:51 -0700

    Update ThemeContext.js

--- src/context/ThemeContext.js
+++ src/context/ThemeContext.js
@@ -1,17 +1,20 @@
 import React, { createContext, useState, useEffect } from "react";
 
+// Permite compartir el modo de tema (claro u oscuro) en toda la app
 export const ThemeContext = createContext();
 
 export const ThemeProviderCustom = ({ children }) => {
   const storedTheme = localStorage.getItem("theme") || "light";
   const [themeMode, setThemeMode] = useState(storedTheme);
 
+   // Función que alterna entre tema claro y oscuro
   const toggleTheme = () => {
     const newTheme = themeMode === "light" ? "dark" : "light";
     setThemeMode(newTheme);
     localStorage.setItem("theme", newTheme);
   };
 
+  // Asegura que el tema actual siempre se mantenga persistente en localStorage
   useEffect(() => {
     localStorage.setItem("theme", themeMode);
   }, [themeMode]);
